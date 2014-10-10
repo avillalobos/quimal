39f71a6f19ad47312a4804da31cc7fb28ae13d42
avillalobos <aevr007@gmail.com>
2014-10-10 01:44:09 -0300

    Added emergency open! and some little configurations

--- src/dome/src/controller/client/dome_controller_client.py
+++ src/dome/src/controller/client/dome_controller_client.py
@@ -7,6 +7,11 @@ from dome.msg import *
 
 class DomeControllerClient:
 
+	def __init__(self):
+		# It is not possible to init node from this archive, so the init node has been executed on the GUI application
+		#rospy.init_node("dome_controller_client", disable_signals=True)
+		self.pub = rospy.Publisher('dome/roof/status', roof, queue_size=100)
+
 	def try_open(self, speed):
 		rospy.wait_for_service('open_dome')
 		try:
@@ -24,3 +29,13 @@ class DomeControllerClient:
 			return roof_msg
 		except rospy.ServiceException, e:
 			print "Service call failed: %s", e
+
+	def emergency_stop(self):
+		# Added some fake information, just to send the Emergency Stop command
+		roof_emergency_msg = roof()
+		roof_emergency_msg.ubication = 0
+		roof_emergency_msg.sensor1 = False
+		roof_emergency_msg.sensor2 = False
+		roof_emergency_msg.sensor3 = False
+		roof_emergency_msg.state = "EMERGENCY_STOP"
+		self.pub.publish(roof_emergency_msg)
