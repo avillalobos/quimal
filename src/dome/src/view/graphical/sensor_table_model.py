0efd2d40e4a01c8fde07b03f4a1cf6ae50c47713
avillalobos <aevr007@gmail.com>
2014-11-08 00:47:28 -0300

    Adding some listing functionalities on selected items to the table model

--- src/dome/src/view/graphical/sensor_table_model.py
+++ src/dome/src/view/graphical/sensor_table_model.py
@@ -13,15 +13,20 @@ class SensorTableModel(QTableWidget):
 		header = self.horizontalHeader()
 		header.setStretchLastSection(True)
 		#self.clicked.connect(self.update)
+		self.list_selected_sensors = set()
+		self.itemClicked.connect(self.clicked_list_handler)
 		    
 	def setmydata(self):
-
 		horHeaders = []
 		for n, key in enumerate(sorted(self.data.keys())):
 			horHeaders.append(key)
 			for m, item in enumerate(self.data[key]):
 				newitem = QTableWidgetItem(item)
-				newitem.setFlags(Qt.ItemIsEnabled)
+				if n == 0:
+					newitem.setFlags(Qt.ItemIsEnabled | Qt.ItemIsUserCheckable) 
+					newitem.setCheckState(Qt.Unchecked)
+				else:
+					newitem.setFlags(Qt.ItemIsEnabled)
 				self.setItem(m, n, newitem)
 				self.setHorizontalHeaderLabels(horHeaders)
 	
@@ -30,3 +35,11 @@ class SensorTableModel(QTableWidget):
 		self.setItem(row, 1, QTableWidgetItem(str(status)))
 		self.setItem(row, 2, QTableWidgetItem(str(value)))
 
+	def clicked_list_handler(self, item):
+		if item.checkState() == Qt.Checked:
+			self.list_selected_sensors.add(str(item.text()))
+
+	def getSelectedItems(self):
+		tmp = list(self.list_selected_sensors)
+		self.list_selected_sensors = set()
+		return tmp
