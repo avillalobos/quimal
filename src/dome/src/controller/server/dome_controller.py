721c67c3a5d2bc9350c612e848fa118b98ce5240
avillalobos <aevr007@gmail.com>
2014-12-29 20:59:57 -0300

    Adding arduino communication to dome_controller

--- src/dome/src/controller/server/dome_controller.py
+++ src/dome/src/controller/server/dome_controller.py
@@ -2,14 +2,14 @@
 
 from dome.srv import *
 from dome.msg import roof
-import rospy, time
+import rospy, time, serial
 
 class DomeController:
 
 	def __init__(self):
 		self.pub = rospy.Publisher('dome/roof/status', roof, queue_size=100)
 		rospy.init_node('ROOF_Controller')
-		self.STATES=["PREPARING","STARTING","OPENING","OPEN", "CLOSING", "PARKING", "PARKED", "CLOSED", "EMERGENCY_STOP"]
+		self.STATES=["PREPARING","STARTING","OPENING","OPEN", "CLOSING", "PARKING", "PARKED", "CLOSED","FINISH", "EMERGENCY_STOP"]
 		roof_initial_msg = self.getInitialState()
 		self.roof_status = None
 		self.last_roof_status = None
@@ -18,6 +18,8 @@ class DomeController:
 			rospy.loginfo("waiting to store first message of roof status")
 			self.pub.publish(roof_initial_msg)
 			time.sleep(1)
+		# TODO fix this part to match with the corresponding arduino and fixing the timeout
+		self.arduino = serial.Serial("/dev/ttyACM0", timeout=30)
 
 	# this code must be programated with the electronics and mechanical team.
 	def getInitialState(self):
@@ -30,9 +32,26 @@ class DomeController:
 		self.pub.publish(roof_initial_msg)
 		return roof_initial_msg
 	
+	def readLineFromArduino(self):
+		msg = ""
+		b = self.arduino.read()
+		while b != "#":
+			msg = msg + b
+			b = self.arduino.read()
+		return msg
+
+	def createRoofMSG(self,status, sensor1, sensor2, sensor3, ubication):
+		roof_msg = roof()
+		roof_msg.ubication = ubication
+		roof_msg.sensor1 = True
+		roof_msg.sensor2 = False
+ 		roof_msg.sensor3 = True
+ 		roof_msg.state = status
+		return roof_msg
+
 	# Callback to receive the status of the Dome at any time
 	def callback(self, data):
-		if data.state == self.STATES[8]:
+		if data.state == self.STATES[9]:
 			rospy.logwarn("RECEIVED AN EMERGENCY STOP!, DOME WILL STOP AS SOON AS POSSIBLE!")
 		else:
 			rospy.loginfo("received new status: " + str(data.state))
@@ -53,24 +72,29 @@ class DomeController:
 			roof_msg.state = "Not possible to open due roof is in another state: " + self.roof_status.state
 			rospy.logwarn(roof_msg.state)
 			return roof_msg
-	
+
 	# perform the actions to open the dome
 	def action_open(self):
 		roof_msg = roof()
-		for i in [0,1,2,3]:
-			if self.roof_status.state != self.STATES[8]:
-				roof_msg.ubication = i
-				roof_msg.sensor1 = True
-				roof_msg.sensor2 = False
-				roof_msg.sensor3 = True
-				roof_msg.state=self.STATES[i]
+		msg = self.arduino.write("openDome")
+		msg = self.readLineFromArduino()
+		while msg != "FINISH":
+			if self.roof_status.state != self.STATES[9]:
+				roof_msg = self.createRoofMSG(msg,True,False,True,1)
 				self.pub.publish(roof_msg)
+				rospy.loginfo(msg)
 				# This frequency should be modified once the hardware is connected
-				r = rospy.Rate(0.5) # 10hz
+				r = rospy.Rate(0.5)
 				r.sleep()
 			else:
 				rospy.logwarn("Due an Emergency stop message, the opening of the dome has been stopped!")
 				break;
+			msg = self.readLineFromArduino()
+		
+		rospy.loginfo("Roof status: " + msg)
+		# getting the last message
+		msg = self.readLineFromArduino()	
+		rospy.loginfo("Roof final state: " + msg)
 		return roof_msg
 
 	# Handler registered when the close service is requested
@@ -90,20 +114,23 @@ class DomeController:
 
 	# perform the actions to close the dome
 	def action_close(self):
-		roof_msg = roof()
-		for i in [4,5,6,7]:
+		msg = self.arduino.write("closeDome")
+		msg = self.readLineFromArduino()
+		while msg != "FINISH":
 			if self.roof_status.state != self.STATES[8]:
-				roof_msg.ubication = i
-				roof_msg.sensor1 = True
-				roof_msg.sensor2 = False
-				roof_msg.sensor3 = True
-				roof_msg.state=self.STATES[i]
+				roof_msg = self.createRoofMSG(msg,False,True,False,1)
 				self.pub.publish(roof_msg)
 				r = rospy.Rate(0.5) # 10hz
 				r.sleep()
 			else:
 				rospy.logwarn("Due an Emergency stop message, the close of the dome has been stopped!")
 				break;
+			msg = self.readLineFromArduino()
+
+		rospy.loginfo("Roof status: " + msg)
+		# getting the last message
+		msg = self.readLineFromArduino()	
+		rospy.loginfo("Roof final state: " + msg)
 		return roof_msg
 
 	# Starte the server to answer the different services handled	
